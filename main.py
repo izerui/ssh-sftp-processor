f64cc720e9866b915c24e8680c8c05a8e8a9fd8b
serv <liuyuhua69@gmail.com>
2023-04-22 18:09:52 +0800

    1

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import asyncio
 import logging
 import time
 
 from remote import Host
+import logging
+import time
 
+from remote import Host
 
 LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
 logging.basicConfig(format=LOG_FORMAT, level=logging.INFO)
@@ -13,9 +15,8 @@ if __name__ == '__main__':
     host = Host('remote01')
     host.connect()
     s_time = time.time()
-    host.get_file('/data/files', 'download/post2')
+    host.get_file('/data/files', 'download/post')
     e_time = time.time()
     log.info(f'下载 耗时: {e_time - s_time}秒')
     print('下载完毕')
     host.close()
-
